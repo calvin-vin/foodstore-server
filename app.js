f0549635559d533a72fb1ff5293aa9dc06e7f2d1
calvin-vin <calvinsan123@gmail.com>
2021-12-31 14:22:51 +0700

    feat: add cors middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -32,6 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use(decodeToken());
 
