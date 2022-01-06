964f3f2d12f1b82f766bc2e52de735bab59e9905
calvin-vin <calvinsan123@gmail.com>
2022-01-06 17:12:32 +0700

    style(region): change response property name

--- app/region/controller.js
+++ app/region/controller.js
@@ -3,52 +3,52 @@ const path = require("path");
 const { StatusCodes } = require("http-status-codes");
 
 const getAllProvinces = async (req, res) => {
-  const provinces = path.resolve(__dirname, "./data/provinces.csv");
+  const data = path.resolve(__dirname, "./data/provinces.csv");
 
-  const data = await csv().fromFile(provinces);
-  res.status(StatusCodes.OK).json({ data });
+  const provinces = await csv().fromFile(data);
+  res.status(StatusCodes.OK).json({ provinces });
 };
 
 const getRegencies = async (req, res) => {
-  const regencies = path.resolve(__dirname, "./data/regencies.csv");
+  const data = path.resolve(__dirname, "./data/regencies.csv");
 
   const { province_code } = req.query;
-  const data = await csv().fromFile(regencies);
+  const regencies = await csv().fromFile(data);
 
-  if (!province_code) return res.status(StatusCodes.OK).json({ data });
+  if (!province_code) return res.status(StatusCodes.OK).json({ regencies });
 
-  const filteredRegencies = data.filter(
+  const filteredRegencies = regencies.filter(
     (regency) => regency.province_code === province_code
   );
-  return res.status(StatusCodes.OK).json({ data: filteredRegencies });
+  return res.status(StatusCodes.OK).json({ regencies: filteredRegencies });
 };
 
 const getDistricts = async (req, res) => {
-  const districts = path.resolve(__dirname, "./data/districts.csv");
+  const data = path.resolve(__dirname, "./data/districts.csv");
 
   const { regency_code } = req.query;
-  const data = await csv().fromFile(districts);
+  const districts = await csv().fromFile(data);
 
-  if (!regency_code) return res.status(StatusCodes.OK).json({ data });
+  if (!regency_code) return res.status(StatusCodes.OK).json({ districts });
 
-  const filteredDistricts = data.filter(
+  const filteredDistricts = districts.filter(
     (district) => district.regency_code === regency_code
   );
-  return res.status(StatusCodes.OK).json({ data: filteredDistricts });
+  return res.status(StatusCodes.OK).json({ districts: filteredDistricts });
 };
 
 const getVillages = async (req, res) => {
-  const villages = path.resolve(__dirname, "./data/villages.csv");
+  const data = path.resolve(__dirname, "./data/villages.csv");
 
   const { district_code } = req.query;
-  const data = await csv().fromFile(villages);
+  const villages = await csv().fromFile(data);
 
-  if (!district_code) return res.status(StatusCodes.OK).json({ data });
+  if (!district_code) return res.status(StatusCodes.OK).json({ villages });
 
-  const filteredVillages = data.filter(
+  const filteredVillages = villages.filter(
     (village) => village.district_code === district_code
   );
-  return res.status(StatusCodes.OK).json({ data: filteredVillages });
+  return res.status(StatusCodes.OK).json({ villages: filteredVillages });
 };
 
 module.exports = {
